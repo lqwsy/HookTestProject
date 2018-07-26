20f819caf32495aa4056bcc9a15d8da4782d06c4
lqwsy <2567924198@qq.com>
2018-07-26 16:33:36 +0800

    添加hook变量，全局变量，构造方法，私有公有方法，调用私有方法

--- src/com/guru/hooktestproject/MyXposedEntrance.java
+++ src/com/guru/hooktestproject/MyXposedEntrance.java
@@ -1,5 +1,8 @@
 package com.guru.hooktestproject;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -16,7 +19,10 @@ public class MyXposedEntrance implements IXposedHookLoadPackage {
 		if(lpparam.packageName.equals(PACKAGE_NAME)){
 			Log.i(TAG,"----------find hook packageName = " + lpparam.packageName);
 			hookLoginResult(lpparam,"com.example.ghjkl.MainActivity","ifLoginSuccess");
-			hookEmptyConstructor(lpparam,"com.example.ghjkl.MainActivity");
+//			hookEmptyConstructor(lpparam,"com.example.ghjkl.MainActivity");
+//			hookParamConstructor(lpparam,"com.example.ghjkl.Userinfo");
+//			hookChangeStaticPrivateValue(lpparam,"com.example.ghjkl.Userinfo");
+			hookPublicMethod(lpparam,"com.example.ghjkl.Userinfo");
 		}
 	}
 	
@@ -38,29 +44,85 @@ public class MyXposedEntrance implements IXposedHookLoadPackage {
 		});
 	}
 	
-	//hook无参构造函数
+	/**修改类静态私有变量*/
+	private static void hookChangeStaticPrivateValue(LoadPackageParam lpparam,String className){
+		final Class<?> clazz = XposedHelpers.findClass(className, lpparam.classLoader);
+		Log.i(TAG,"hook hookChangeStaticPrivateValue");
+		XposedHelpers.setStaticIntField(clazz, "CODE", 666);
+	}
+	
+	/**hook无参构造函数，修改私有变量*/
 	private static void hookEmptyConstructor(LoadPackageParam lpparam,String className){
 		XposedHelpers.findAndHookConstructor(className, lpparam.classLoader, new XC_MethodHook(){
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param)
 					throws Throwable {
 				super.beforeHookedMethod(param);
-				Log.i(TAG,"hook constructor before");
+				Log.i(TAG,"hook Emptyconstructor before");
 				//此时对象还没建立，不能获取对象param.thisObject ，不能修改非静态变量
 			}
 			@Override
 			protected void afterHookedMethod(MethodHookParam param)
 					throws Throwable {
 				super.afterHookedMethod(param);
-				Log.i(TAG,"hook constructor after");
+				Log.i(TAG,"hook Emptyconstructor after");
 				XposedHelpers.setIntField(param.thisObject,"testInt", 99);
 			}
 		});
 	}
 
-	//hook有参构造函数
-	private static void hookParamConstructor(){
-		
+	/**hook有参构造函数,修改参数*/
+	private static void hookParamConstructor(LoadPackageParam lpparam,String className){
+		XposedHelpers.findAndHookConstructor(className, lpparam.classLoader,int.class,String.class,String.class,new XC_MethodHook(){
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				super.beforeHookedMethod(param);
+				Log.i(TAG,"hook Paramconstructor before");
+			}
+		});
+	}
+	
+	/**
+	 * hook公有方法
+	 * 修改参数
+	 * 修改private和public的全局变量
+	 * 调用private函数
+	 * */
+	private static void hookPublicMethod(final LoadPackageParam lpparam,final String className){
+		XposedHelpers.findAndHookMethod(className, lpparam.classLoader, "setUserName",String.class, new XC_MethodHook(){
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				super.beforeHookedMethod(param);
+				param.args[0] = "stupid";
+				XposedHelpers.setIntField(param.thisObject,"publicValue", 101);
+				XposedHelpers.setIntField(param.thisObject, "privateValue", 102);
+				Class<?> clazz = XposedHelpers.findClass(className, lpparam.classLoader);
+				Method method = clazz.getDeclaredMethod("getPirvateValue", String.class);
+				method.setAccessible(true);
+				//用反射的方式调用
+//				method.invoke(param.thisObject, "testValue by invoke");
+				//用hook的对象
+//				XposedHelpers.callMethod(param.thisObject, "getPirvateValue", "testValue by param.thisObject");
+				//contructor构造对象
+				Constructor contructor = clazz.getConstructor();
+				XposedHelpers.callMethod(contructor.newInstance(), "getPirvateValue", "testValue by contructor");
+			}
+		});
+	}
+	
+	/**
+	 * hook私有方法
+	 * 修改参数
+	 * */
+	private static void hookPrivateMethod(final LoadPackageParam lpparam,final String className){
+		XposedHelpers.findAndHookMethod(className, lpparam.classLoader, "setUserName",String.class, new XC_MethodHook(){
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				super.beforeHookedMethod(param);
+				
+			}
+		});
 	}
 	
 	
